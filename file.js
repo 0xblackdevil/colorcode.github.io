dfb5d492ff9d943cc70bc3993536294fac083c64
zinzuvadiya Meet <zinzuvadiyameet98@gmail.com>
2019-03-20 10:10:30 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,4 +1,6 @@
-function dis(num){
-  document.getElementById("textview").value = num
+function dis(num,rgb){
+  document.getElementById("textview").textContent = num.toUpperCase()
   document.getElementById("block").style.background = num
+
+  document.getElementById("rgbv").textContent = rgb
 }
